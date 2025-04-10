51540e26b09e870108195c832789ff74402418fe
Thaibao123xyz <thaibao123xyz@gmail.com>
2025-04-10 23:29:35 +0700

    them nhe cai lauout chu h t met lam roi

--- app/views/login/admin_dashboard.php
+++ app/views/login/admin_dashboard.php
@@ -1,15 +1,41 @@
 <?php include __DIR__ . '/../layout/header.php'; ?>
 
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Quản trị viên</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Chu Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<style>
+    body {
+        margin: 0;
+        height: 100vh;
+        font-family: Arial, sans-serif;
+        background-image: url('/du_an/8XBET/public/img_admin_login/1742660101_download.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
 
-<body class="bg-dark text-white">
-    <div class="container mt-5">
+    h2 {
+        color: white;
+        text-align: center;
+        margin-top: 250px;
+    }
+
+    p {
+        color: white;
+        text-align: center;
+        font-size: 20px;
+    }
+</style>
+
+<body>
+    <div class="container mt-1">
         <h2>Chào quản trị viên, <?= $_SESSION['user'] ?>!</h2>
         <p>Đây là trang quản trị hệ thống.</p>
     </div>
