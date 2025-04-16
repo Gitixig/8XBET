ba13f056f1e1aca41c1ee37b05a4ad2629328b7d
Thaibao123xyz <thaibao123xyz@gmail.com>
2025-04-16 20:03:20 +0700

    t sua lai file menu va dg lam add player nhung chua xong

--- app/views/main-menu/Menu.php
+++ app/views/main-menu/Menu.php
@@ -62,41 +62,40 @@ require_once __DIR__ . '/../../../config/config.php';
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="bi bi-list fs-2"></i>
             </button>
-
             <a class="navbar-brand d-flex align-items-center" href="#">
-                <img src="../../../public/img/img-logo/ChatGPT Image 14_46_48 12 thg 4, 2025.png" style="width: 70px;" alt="Logo">
+                <img src="/du_an/8XBET/public/img/img-logo/logo.png" style="width: 70px;" alt="Logo">
             </a>
             <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                 <ul class="navbar-nav text-center">
-                <li class="nav-item"><a class="nav-link text-white active" href="/du_an/8XBET/app/views/Home/Home.php"><b>HOME</b></a></li>
+                    <li class="nav-item"><a class="nav-link text-white active" href="/du_an/8XBET/app/views/Home/Home.php"><b>HOME</b></a></li>
                     <li class="nav-item"><a class="nav-link text-white active" href="#"><b>TTCN</b></a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="#"><b>CẦU THỦ</b></a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="#"><b>CLB</b></a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="#"><b>SÂN VẬN ĐỘNG</b></a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="#"><b>HLV</b></a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="#"><b>TIN TỨC</b></a></li>
-                    <?php if (!isset($_SESSION['user'])){?>  
+                    <?php if (!isset($_SESSION['user'])) { ?>
                         <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <b style="color: white;">SIGN IN</b>
-                        </a>
-                        <ul class="dropdown-menu" style="background-color:rgb(240, 13, 13);">
-                            <li><a class="dropdown-item" href="/du_an/8XBET/app/views/login/login.php"> <b style="color: white;">LOGIN</b></a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="/du_an/8XBET/app/views/add_User/add_User.php"> <b style="color: white;">SIGN UP</b></a></li>
-                            
-                        </ul>
-                    </li>
-                <?php }?>
-                <?php if(isset($_SESSION['role'])==="admin"){?>
-                    <li class="nav-item"><a class="nav-link text-white" href="/du_an/8XBET/app/views/List_player/list_player.php"><b>Add player</b></a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="#"><b>Add CLB</b></a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="#"><b>Add stadium</b></a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="#"><b>Add HLV</b></a></li>
-                    <?php }?>
-            </ul>
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <b style="color: white;">SIGN IN</b>
+                            </a>
+                            <ul class="dropdown-menu" style="background-color:rgb(240, 13, 13);">
+                                <li><a class="dropdown-item" href="/du_an/8XBET/app/views/login/login.php"> <b style="color: white;">LOGIN</b></a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="/du_an/8XBET/app/views/add_User/add_User.php"> <b style="color: white;">SIGN UP</b></a></li>
+
+                            </ul>
+                        </li>
+                    <?php } ?>
+                    <?php if (isset($_SESSION['role']) === "admin") { ?>
+                        <li class="nav-item"><a class="nav-link text-white" href="/du_an/8XBET/app/views/List_player/list_player.php"><b>Add player</b></a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#"><b>Add CLB</b></a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#"><b>Add stadium</b></a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#"><b>Add HLV</b></a></li>
+                    <?php } ?>
+                </ul>
             </div>
             <a class="nav-link text-white me-3 d-flex align-items-center" href="../main-menu/Search.php">
                 <b>Search</b>
