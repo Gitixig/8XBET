49797f26ca153766d2ff15fb47a75a272a8b0993
Thaibao123xyz <thaibao123xyz@gmail.com>
2025-04-16 22:56:18 +0700

    them cai footer

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,12 +1,20 @@
-<footer>
-    <div class="container">
-        <img src="https://fireart.studio/wp-content/uploads/2018/01/lametric-footer-1.jpg" style="width:100%">
+<footer style="background-color: #B22222; color: #eee; padding: 40px 0; text-align: center; font-family: sans-serif;">
+    <h2 style="margin-bottom: 10px; font-size: 30px;">8XBET.com</h2>
+    <p style="font-size: 20px;">Thành lập năm 2025 </p>
+    <p style="font-size: 20px;">"Đây là sân chơi cảm xúc – nơi tiền chỉ có lượt đi !"</p>
+    <div style="margin: 20px 0; font-size: 30px;">
+        <a href="/players" style="color: #eee; margin: 0 10px;">Cầu thủ</a>
+        <a href="/coaches" style="color: #eee; margin: 0 10px;">HLV</a>
+        <a href="/stadiums" style="color: #eee; margin: 0 10px;">Sân vận động</a>
+        <a href="/schedule" style="color: #eee; margin: 0 10px;">Lịch thi đấu</a>
+        <a href="/news" style="color: #eee; margin: 0 10px;">Tin tức</a>
     </div>
+
+    <div style="margin: 0px 0; font-size: 50px;">
+        <a href="#"><img src="../layout/footer_logo/fb.png" alt="Facebook" width="24" /></a>
+        <a href="#"><img src="../layout/footer_logo/Youtube_logo.png" alt="YouTube" width="24" /></a>
+        <a href="#"><img src="../layout/footer_logo/inta.png" alt="Instagram" width="24" /></a>
+    </div>
+
+    <p style="margin-top: 20px; font-size: 20px;">© 2025 8XBET. All rights reserved.</p>
 </footer>
-
-<script src="//code.jquery.com/jquery.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/5.3.3/js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
