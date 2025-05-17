a79c318abfe7034da40ffc43e776424766812fc2
Gitixig <hatuandat190290@gmail.com>
2025-05-17 09:51:54 +0700

    hdhawuihdiwqh

--- app/controllers/CheckoutController.php
+++ app/controllers/CheckoutController.php
@@ -72,11 +72,11 @@ class CheckoutController
                 if (sendOrderConfirmationEmailToAdmin($emailData)) {
                     $_SESSION['success_message'] = "Đơn hàng của bạn đã được đặt thành công. Thông tin chi tiết đã được gửi đến email của bạn và admin.";
                     unset($_SESSION['cart']); // Xóa giỏ hàng sau khi đặt hàng thành công
-                    header('Location: thank-you'); // Chuyển hướng đến trang cảm ơn
+                    header('Location: /du_an/8Xbet/app/views/Order/thank-you.php'); // Chuyển hướng đến trang cảm ơn
                     exit();
                 } else {
                     $_SESSION['error_message'] = "Đã có lỗi xảy ra khi gửi thông báo đến admin. Vui lòng liên hệ lại sau.";
-                    header('Location: thank-you'); // Chuyển hướng trở lại trang thanh toán
+                    header('Location: checkout'); // Chuyển hướng trở lại trang thanh toán
                     exit();
                 }
             } catch (Exception $e) {
--- app/views/Order/checkout_success.php
+++ app/views/Order/checkout_success.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang Thanh Toán</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap">
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         body {
             font-family: 'Inter', sans-serif;
